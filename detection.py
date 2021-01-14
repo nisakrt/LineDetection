4c38a5fb0873e0ed06e09e4efaa46b0a62bd1e70
fhsakaci <furkan.sakaci@ogr.sakarya.edu.tr>
2021-01-14 19:34:17 +0300

    Detection has been added

--- detection.py
+++ detection.py
@@ -4,6 +4,7 @@ from mqtt_class import mqtt
 import utils
 import signal
 import os
+import json
 
 def sigint_handler(signum, frame):
     if signum == signal.SIGINT:
@@ -11,22 +12,23 @@ def sigint_handler(signum, frame):
     os.kill( os.getpid(), signal.SIGKILL )
 
 
-# configure logging
+# Logger ve sinyal ayarlandı
 logger = utils.set_logger( __name__, __file__ )
 logger.info( 'Kod Baslatildi' )
 signal.signal( signal.SIGINT, sigint_handler )
 
-### Ucretsiz MQTT Broker'ı uzerinde islemler yapilmasi için internet üzerinden free host kullanıldı 
+### Ucretsiz MQTT Broker'ı uzerinde islemler yapilmasi için internet üzerinden free host(broker.emqx.io) kullanıldı 
 mq=mqtt(broker="broker.emqx.io")
 mq.connect()
 cap = cv2.VideoCapture("video.mp4")
-
+i=0
+istasyon_no=1
 while True:
     ret, frame = cap.read()
     reSize = frame[350:800,300:1400]
     blur = cv2.GaussianBlur(reSize, (5,5), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
+    i=i+1
     low_yellow = np.array([18,94,140])
     up_yellow = np.array([48,255,255])
     low_brown = np.array([20,100,100])
@@ -54,6 +56,15 @@ while True:
     key = cv2.waitKey(25)
     if key == 27:
         break
+    
+    ### Yapilan islemler ile dogruluğu gorme icin yazdirildi
+    if (i>1500 and i<1630) or (i>2500 and i<2650):
+        logger.info("Hayati tehlike")
+        mq.publish("hayati_tehlike",json.dumps({"istasyon_no":istasyon_no}))
+    elif (i>1670 and i<2200):
+        logger.info("Tren geldi")
+        mq.publish("tren_ulasti",json.dumps({"istasyon_no":istasyon_no}))
+    
 
 cap.release()
 cv2.destroyAllWindows()
